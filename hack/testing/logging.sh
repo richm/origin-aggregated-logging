1814abba3aaaa0d6cb7f2e2252c268375bad799a
Rich Megginson <rmeggins@redhat.com>
2016-06-30 10:32:42 -0600

    add some debugging

--- hack/testing/logging.sh
+++ hack/testing/logging.sh
@@ -417,7 +417,7 @@ function wait_for_app() {
   wait_for_url_timed "http://${FRONTEND_IP}:5432" "[INFO] Frontend says: " $((2*TIME_MIN))
 
   echo "[INFO] Testing app"
-  wait_for_command '[[ "$(curl -s -X POST http://${FRONTEND_IP}:5432/keys/foo -d value=1337)" = "Key created" ]]'
+  wait_for_command '[[ "$(curl -s -X POST http://${FRONTEND_IP}:5432/keys/foo -d value=1337)" = "Key created" ]]' 1000 "curl -s -X POST http://${FRONTEND_IP}:5432/keys/foo -d value=1337"
   wait_for_command '[[ "$(curl -s http://${FRONTEND_IP}:5432/keys/foo)" = "1337" ]]'
 }
 
