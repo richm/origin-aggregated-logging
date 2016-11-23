cee0dc3648ebfe72e26bc030d53fd97942b9627a
Rich Megginson <rmeggins@redhat.com>
2016-11-22 17:20:18 -0700

    wip

--- hack/setup-mux.sh
+++ hack/setup-mux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -euxo pipefail
+set -eux
 
 # check required arguments
 
@@ -28,7 +28,7 @@ if [ ! -d $MASTER_CONFIG_DIR ] ; then
     MASTER_CONFIG_DIR=`ps -ef|grep -v awk|awk '/openshift.*master-config=/ {print gensub(/^.*--master-config=(\/.*)\/master-config.yaml.*$/, "\\\1", 1)}'|head -1`
 fi
 
-if [ ! -d $MASTER_CONFIG_DIR/ca.key ] ; then
+if [ ! -f $MASTER_CONFIG_DIR/ca.key ] ; then
     echo Error: could not find the openshift ca key needed to create the mux server cert
     echo Check your permissions - you may need to run this script as root
     echo Otherwise, please specify MASTER_CONFIG_DIR correctly and re-run this script
@@ -51,18 +51,27 @@ openshift admin ca create-server-cert  \
 # generate mux shared_key
 cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1 > "$workdir/mux-shared-key"
 
+oc delete -n logging dc logging-mux || :
+oc delete -n logging secret logging-mux || :
+oc delete -n logging configmap logging-mux || :
+oc delete -n logging service logging-mux || :
+oc delete -n logging route logging-mux || :
+
 # add secret for mux
-oc secrets new logging-mux \
-   mux-key=$dir/mux-internal.key mux-cert=$dir/mux-internal.crt \
-   mux-shared-key=$dir/mux-shared-key
+oc secrets -n logging new logging-mux \
+   mux-key=$workdir/mux.key mux-cert=$workdir/mux.crt \
+   mux-shared-key=$workdir/mux-shared-key
 
 # add mux secret to fluentd service account
-oc secrets add serviceaccount/aggregated-logging-fluentd \
+oc secrets -n logging add serviceaccount/aggregated-logging-fluentd \
    logging-fluentd logging-mux
 
 # create our configmap files
 cat > $workdir/fluent.conf <<EOF
-@include configs.d/openshift/system.conf
+#@include configs.d/openshift/system.conf
+<system>
+  log_level trace
+</system>
 @include configs.d/openshift/input-pre-*.conf
 @include configs.d/user/forward.conf
 @include configs.d/openshift/input-post-*.conf
@@ -92,10 +101,10 @@ cat > $workdir/forward.conf <<EOF
 <source>
   @type secure_forward
   @label @INGRESS
-  port "#{ENV['FORWARD_LISTEN_PORT'] || '24284'}"
+  port ${FORWARD_LISTEN_PORT:-24284}
   # bind 0.0.0.0 # default
-  log_level "#{ENV['FORWARD_INPUT_LOG_LEVEL'] || ENV['LOG_LEVEL'] || 'warn'}"
-  self_hostname "#{ENV['FORWARD_LISTEN_HOST'] || 'mux.example.com'}"
+  log_level ${FORWARD_INPUT_LOG_LEVEL:-warn}
+  self_hostname $MUX_HOST
   shared_key    "#{File.open('/etc/fluentd/muxkeys/mux-shared-key') do |f| f.readline end.rstrip}"
   secure yes
   cert_path        /etc/fluent/muxkeys/mux-cert
@@ -105,42 +114,34 @@ cat > $workdir/forward.conf <<EOF
 EOF
 
 # generate fluentd configmap
-oc create configmap logging-fluentd \
+oc create -n logging configmap logging-mux \
    --from-file=fluent.conf=$workdir/fluent.conf \
    --from-file=forward.conf=$workdir/forward.conf
 oc label configmap/logging-mux logging-infra=support
 
 # generate the mux template from the fluentd template
-oc get template logging-fluentd-template -o yaml > $workdir/fluentd.yaml
+oc get -n logging template logging-fluentd-template -o yaml > $workdir/fluentd.yaml
 
 # create snippet files that we can insert with sed
 cat > $workdir/1 <<EOF
-  -
-    description: 'The default port the forward listener uses for incoming connections (targetPort: mux-forward).'
-    name: FORWARD_LISTEN_PORT
-    value: \${FORWARD_LISTEN_PORT_DEFAULT}
+    replicas: 1
+    selector:
+      component: mux
+      provider: openshift
+    strategy:
+      resources: {}
+      rollingParams:
+        intervalSeconds: 1
+        timeoutSeconds: 600
+        updatePeriodSeconds: 1
+      type: Rolling
+    template:
 EOF
 
 cat > $workdir/2 <<EOF
-        provider: openshift
-        component: "mux"
-      triggers: {}
-      strategy:
-        resources: {}
-        rollingParams:
-          intervalSeconds: 1
-          timeoutSeconds: 600
-          updatePeriodSeconds: 1
-        type: Rolling
-EOF
-
-cat > $workdir/3 <<EOF
-            ports:
-            - containerPort: \${FORWARD_LISTEN_PORT}
-              name: "mux-forward"
-EOF
-
-cat > $workdir/4 <<EOF
+          ports:
+          - containerPort: \${FORWARD_LISTEN_PORT}
+            name: mux-forward
           volumeMounts:
           - mountPath: /etc/fluent/configs.d/user
             name: config
@@ -151,10 +152,9 @@ cat > $workdir/4 <<EOF
           - name: muxcerts
             mountPath: /etc/fluent/muxkeys
             readOnly: true
-        nodeSelector:
 EOF
 
-cat > $workdir/5 <<EOF
+cat > $workdir/3 <<EOF
         volumes:
         - configMap:
             name: logging-mux
@@ -165,18 +165,26 @@ cat > $workdir/5 <<EOF
         - name: muxcerts
           secret:
             secretName: logging-mux
-    updateStrategy:
 EOF
 
-cat > $workdir/6 <<EOF
+cat > $workdir/4 <<EOF
 -
   description: 'The external hostname used to connect to the forward listener.'
   name: FORWARD_LISTEN_HOST
   value: "mux.example.com"
 -
   description: 'The default port the forward listener uses for incoming connections (targetPort: mux-forward).'
-  name: FORWARD_LISTEN_PORT_DEFAULT
+  name: FORWARD_LISTEN_PORT
   value: "24284"
+EOF
+
+cat > $workdir/5 <<EOF
+          - name: FORWARD_LISTEN_HOST
+            value: \${FORWARD_LISTEN_HOST}
+          - name: FORWARD_LISTEN_PORT
+            value: \${FORWARD_LISTEN_PORT}
+          - name: VERBOSE
+            value: "true"
 EOF
 
 cp $workdir/fluentd.yaml $workdir/mux.yaml
@@ -188,27 +196,24 @@ sed -i -e s/logging-fluentd-template-maker/logging-mux-template-maker/ \
     -e "s/component: fluentd/component: mux/" \
     -e "s,apiVersion: extensions/v1beta1,apiVersion: v1," \
     -e 's/kind: "DaemonSet"/kind: "DeploymentConfig"/' \
-    -e 's/component: "fluentd"/component: "mux"/' \
-    -e 's/name: "logging-fluentd"/name: "logging-mux"/' \
+    -e 's/kind: DaemonSet/kind: DeploymentConfig/' \
+    -e 's/component: fluentd/component: mux/' \
+    -e 's/name: logging-fluentd/name: logging-mux/' \
     -e "s/name: fluentd-elasticsearch/name: mux/" \
-    -e "/  parameters:/r $workdir/1" \
-    -e "/    spec:/a\
-      replicas: 1" \
-    -e "/        matchLabels:/,/          minReadySeconds:/d" \
-    -e "/      selector:/r $workdir/2" \
-    -e "/            imagePullPolicy: Always/r $workdir/3" \
-    -e "/          securityContext:/,/            privileged: true/d" \
-    -e "/          volumeMounts:/,/        nodeSelector:/d" \
-    -e "/        serviceAccountName: aggregated-logging-fluentd/r $workdir/4" \
-    -e "/        volumes:/,/    updateStrategy:/d" \
-    -e "/        terminationGracePeriodSeconds:/r $workdir/5" \
-    -e "/^parameters:/r $workdir/6" \
+    -e "/^    selector:/,/^    template:/d" \
+    -e "/^  spec:/r $workdir/1" \
+    -e "/^          volumeMounts:/,/^        nodeSelector:/c\        nodeSelector:" \
+    -e "/^          securityContext:/,/^            privileged: true/d" \
+    -e "/^          imagePullPolicy: Always/r $workdir/2" \
+    -e "/^        volumes:/,/^    updateStrategy:/c\    updateStrategy:" \
+    -e "/^        terminationGracePeriodSeconds:/r $workdir/3" \
+    -e "/^parameters:/r $workdir/4" \
+    -e "/^        - env:/r $workdir/5" \
     $workdir/mux.yaml
 
-oc new-app --param=FORWARD_LISTEN_HOST=$MUX_HOST -f $workdir/mux.yaml
-oc new-app logging-mux-template
+oc new-app -n logging --param=FORWARD_LISTEN_HOST=$MUX_HOST -f $workdir/mux.yaml
 
-oc create service clusterip logging-mux --tcp=24284:mux-forward
+oc create -n logging service clusterip logging-mux --tcp=$FORWARD_LISTEN_PORT:mux-forward
 
-oc create route passthrough --service="logging-mux" \
+oc create -n logging route passthrough --service="logging-mux" \
    --hostname="$MUX_HOST" --port="mux-forward"
